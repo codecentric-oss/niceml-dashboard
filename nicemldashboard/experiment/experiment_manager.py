1233736845bed13f1f2742abccee78385800b151
ihsankisi <ihsank7777@tutanota.com>
2024-04-18 14:48:06 +0200

    refactor: Updated the filter logic to reflect latest changes

--- nicemldashboard/experiment/experiment_manager.py
+++ nicemldashboard/experiment/experiment_manager.py
@@ -22,11 +22,7 @@ class ExperimentManager:
         filtered_experiments = self.experiments.copy()
         for key, value in filters.items():
             try:
-                if isinstance(value, dict):
-                    filtered_experiments = [exp for exp in filtered_experiments
-                                            if all(item in getattr(exp, key, {}).items() for item in value.items())]
-                else:
-                    filtered_experiments = [exp for exp in filtered_experiments if getattr(exp, key, None) == value]
+                filtered_experiments = [exp for exp in filtered_experiments if getattr(exp, key, None) == value]
             except TypeError as e:
                 # Log the error message with details of which experiment and filter caused it.
                 logging.error(f"Incomparable types between attribute '{key}' with value '{value}' "
